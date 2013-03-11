9ba5fdc801d25ae8cf2c05721c238377383f1637
Alex Fedoseev <lxfedoseev@gmail.com>
2013-03-11 17:01:19 +0400

    places

--- gen/com/example/alexfed/raiffstat/R.java
+++ gen/com/example/alexfed/raiffstat/R.java
@@ -54,15 +54,16 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_clear_db=0x7f040004;
-        public static final int menu_make_place=0x7f04000a;
-        public static final int menu_manage_places=0x7f04000b;
+        public static final int menu_make_place=0x7f04000b;
+        public static final int menu_manage_places=0x7f04000c;
         public static final int menu_manage_terminals=0x7f040008;
         public static final int menu_query=0x7f040005;
         public static final int menu_settings=0x7f040002;
         public static final int menu_sms_import=0x7f040003;
         public static final int menu_summary=0x7f040007;
-        public static final int places_name=0x7f040009;
+        public static final int places_name=0x7f04000a;
         public static final int report_name=0x7f040006;
+        public static final int terminals_name=0x7f040009;
     }
     public static final class style {
         /** 
