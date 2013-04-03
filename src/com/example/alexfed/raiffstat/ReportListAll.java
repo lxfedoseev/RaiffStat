3cdc95a736d448c8360114df88bad7ba647d9959
Alex Fedoseev <lxfedoseev@gmail.com>
2013-04-03 15:12:56 +0400

    removed unneeded stuff

--- src/com/example/alexfed/raiffstat/ReportListAll.java
+++ src/com/example/alexfed/raiffstat/ReportListAll.java
@@ -64,9 +64,7 @@ public class ReportListAll extends SherlockListFragment {
 	
 	static final int SUMMARY_ID = Menu.FIRST;
     static final int SORT_ID = Menu.FIRST+1;
-    static final int GRAPH_ID = Menu.FIRST+2;
-    static final int PERIOD_ID = Menu.FIRST+3;
-    static final int TOTALLY_ID = Menu.FIRST+4;
+    static final int PERIOD_ID = Menu.FIRST+2;
     
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -110,19 +108,14 @@ public class ReportListAll extends SherlockListFragment {
 			MenuItem periodItem = menu.add(Menu.NONE, PERIOD_ID, 0, R.string.menu_period);
 			periodItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 		}
-		MenuItem graphItem = menu.add(Menu.NONE, GRAPH_ID, 0, R.string.menu_graph);
-	    graphItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-	    graphItem.setIcon(R.drawable.ic_action_line_chart);
-	    
+    
 		MenuItem summaryItem = menu.add(Menu.NONE, SUMMARY_ID, 0, R.string.menu_summary);
 		summaryItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-		summaryItem.setIcon(R.drawable.ic_action_calculator);
+		summaryItem.setIcon(R.drawable.ic_action_line_chart);
 		
 	    MenuItem sortItem = menu.add(Menu.NONE, SORT_ID, 0, R.string.menu_sort);
 	    sortItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-		
-		MenuItem totallyItem = menu.add(Menu.NONE, TOTALLY_ID, 0, R.string.menu_totally);
-		totallyItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
 		super.onCreateOptionsMenu(menu, inflater);
 	}
 	
@@ -135,34 +128,8 @@ public class ReportListAll extends SherlockListFragment {
             case SORT_ID:
             	doSort();
     			return true;
-            case GRAPH_ID:
-            	doDrawGraph();
-    			return true;
             case PERIOD_ID:
             	showDatePickerDialog(true);
-    			return true;
-            case TOTALLY_ID:
-            	if(dayFrom == null || dayTo == null){
-            		//TODO: toast something
-            		return true;
-            	}
-            	if(transactions.isEmpty()){
-            		//TODO: toast something
-            		return true;
-            	}
-            	DatabaseHandler db = new DatabaseHandler(activity);
-        		List<TransactionEntry> trs = db.getTransactionsForGraph(convertStringDate(dayFrom+ " 00:00:00"), 
-        				convertStringDate(dayTo+ " 23:59:59"), StaticValues.CURR_RUB);
-        		db.close();
-        		if(trs.size()<1){
-        			  Toast.makeText(activity, getResources().getString(R.string.toast_no_values) + " " + StaticValues.CURR_RUB, 
-        					  Toast.LENGTH_LONG).show();
-        			  return true;	  
-        		}
-            	Intent myIntent = new Intent(activity, ReportSummary.class);
-            	myIntent.putExtra("day_from", dayFrom);
-            	myIntent.putExtra("day_to", dayTo);
-            	activity.startActivity(myIntent);
     			return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -217,196 +184,12 @@ public class ReportListAll extends SherlockListFragment {
     	alert.setView(datePicker);
     	alert.show();
     }
-    
-	private void doDrawGraph(){
-		//getActivity();
-		LinearLayout graphLayout = new LinearLayout(activity);
-		AlertDialog.Builder alert = new AlertDialog.Builder(activity);
-		  
-		DatabaseHandler db = new DatabaseHandler(activity);
-		List<TransactionEntry> trs = db.getTransactionsForGraph(convertStringDate(dayFrom+ " 00:00:00"), 
-				convertStringDate(dayTo+ " 23:59:59"), StaticValues.CURR_RUB);
-		if(trs.size()<1){
-			  Toast.makeText(activity, getResources().getString(R.string.toast_no_values) + " " + StaticValues.CURR_RUB, 
-					  Toast.LENGTH_LONG).show();
-			  db.close();
-			  return;
-			  
-		  }
-		
-		Map<Integer, Double> sum = new HashMap<Integer, Double>();
-		Set<Integer> cats = new HashSet<Integer>();
-		double totalSum = 0.0;
-		for (TransactionEntry t : trs) {
-			if(!cats.contains(t.getExpCategory())){
-				cats.add(t.getExpCategory());
-				sum.put(t.getExpCategory(), 0.0);
-			}
-			sum.put(t.getExpCategory(), sum.get(t.getExpCategory())+t.getAmount());
-			totalSum += t.getAmount();
-		}
-		Map<Integer, Double> sortedSum = sortByComparator(sum);
-		
-		GraphViewSeries[] sers = new GraphViewSeries[cats.size()];
-		int i = 0;
-		String catName = "";
-		int catColor = 0xff000000;
-		int thickness = (int)((activity.getResources().getDisplayMetrics().widthPixels)*0.07);//7% of the screen's width
-		double maxVal = Double.MIN_VALUE;
-		double percents = 0.0;
-		
-		for (Map.Entry<Integer, Double> entry : sortedSum.entrySet()) {
-			
-			if(((Integer)entry.getKey()) == StaticValues.EXPENSE_CATEGORY_UNKNOWN){
-				catName = getResources().getString(R.string.str_category_undefined);
-				catColor = 0xff000000;
-			}else{
-				CategoryEntry cat = db.getCategory(((Integer)entry.getKey()));
-				catName = cat.getName();
-				catColor = cat.getColor();
-			}
-			percents = ((Double)entry.getValue()/totalSum)*100;
-			maxVal = Math.max(maxVal, percents);
-			GraphViewData[] data = new GraphViewData[2];
-			data[0] = new GraphViewData(i+1, 0);
-			data[1] = new GraphViewData(i+1, percents);
-			percents = Math.round(percents);
-			if(percents < 0.1){
-				catName = catName + " < 0.1%";
-			}else{
-				int per = (int) percents;
-				catName = catName + " " + per + "%";
-			}
-			sers[i] = new GraphViewSeries(catName, 
-					new GraphViewSeries.GraphViewSeriesStyle(catColor, thickness), data);
-			i++;
-			
-		}
-		/*for(Integer c: cats){
-			if(c == StaticValues.EXPENSE_CATEGORY_UNKNOWN){
-				catName = getResources().getString(R.string.str_category_undefined);
-				catColor = 0xff000000;
-			}else{
-				CategoryEntry cat = db.getCategory(c);
-				catName = cat.getName();
-				catColor = cat.getColor();
-			}
-			percents = (sum.get(c)/totalSum)*100;
-			maxVal = Math.max(maxVal, percents);
-			GraphViewData[] data = new GraphViewData[2];
-			data[0] = new GraphViewData(i+1, 0);
-			data[1] = new GraphViewData(i+1, percents);
-			percents = Math.round(percents);
-			if(percents < 0.1){
-				catName = catName + " < 0.1%";
-			}else{
-				int per = (int) percents;
-				catName = catName + " " + per + "%";
-			}
-			sers[i] = new GraphViewSeries(catName, 
-					new GraphViewSeries.GraphViewSeriesStyle(catColor, thickness), data);
-			i++;
-		}*/
-		
-		db.close();
-
-		LineGraphView graphView = new LineGraphView(activity, "%"){  
-			   @Override  
-			   protected String formatLabel(double value, boolean isValueX) {  
-			      if (isValueX) {
-			    	  /*int i = (int) value;
-			    	  long date = dateList.get(i<=0?0:(i>=count)?count-1:i);
-			    	  String dateString = new SimpleDateFormat("dd/MM/yyyy").format(new Date(date));
-			    	  return dateString;*/
-			    	  return "";
-			      } else {
-			    	  /*double result = value * 10; 
-			    	  result = Math.round(result);
-			    	  result = result / 10;
-			          return ""+result; */
-			    	  
-			          return super.formatLabel(value, isValueX);
-			    	  //return "";
-			      }
-			   }  
-		}; 
-		
-		 int height = (int)activity.getResources().getDisplayMetrics().heightPixels/2;
-		 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, height);
-		 params.rightMargin = (int) convertDpToPixel(5, activity);
-		 params.leftMargin = (int) convertDpToPixel(5, activity);
-		 graphView.setLayoutParams(params);
-		  
-		  graphView.setBackgroundColor(Color.WHITE);
-			for(i=0; i<cats.size();i++){
-				graphView.addSeries(sers[i]);
-			}
-		  
-		  graphView.setViewPort(0,cats.size()+1);
-		  graphView.setScrollable(false);   
-		  graphView.setScalable(false);   
-		  graphView.setManualYAxisBounds(maxVal, 0);
-		  graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.BLACK); 
-		  graphView.getGraphViewStyle().setVerticalLabelsColor(Color.BLACK);
-		  graphView.setShowLegend(true); 
-		  graphView.setLegendAlign(GraphView.LegendAlign.TOP);  
-		  graphView.setLegendWidth(200);
-			
-			//setLabelParams(graphView);
-			
-			graphLayout.addView(graphView); 
-		  
-		  alert.setView(graphLayout);
-		  alert.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-
-			}
-		  });
-
-		  alert.show(); 
-	}
-	
-	//http://www.mkyong.com/java/how-to-sort-a-map-in-java/
-	private static Map sortByComparator(Map unsortMap) {
-		 
-		List list = new LinkedList(unsortMap.entrySet());
- 
-		// sort list based on comparator
-		Collections.sort(list, new Comparator() {
-			public int compare(Object o1, Object o2) {
-				return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue())*(-1);
-			}
-		});
- 
-		// put sorted list into map again
-                //LinkedHashMap make sure order in which keys were inserted
-		Map sortedMap = new LinkedHashMap();
-		for (Iterator it = list.iterator(); it.hasNext();) {
-			Map.Entry entry = (Map.Entry) it.next();
-			sortedMap.put(entry.getKey(), entry.getValue());
-		}
-		return sortedMap;
-	}
-	
-	private float convertDpToPixel(float dp,Context context){
-	    Resources resources = context.getResources();
-	    DisplayMetrics metrics = resources.getDisplayMetrics();
-	    float px = dp * (metrics.densityDpi/160f);
-	    return px;
-	}
-	
+    	
 	private void inflateList(){
 		getListView().setDivider(null);
 		queryDateIntervalPlace(convertStringDate(dayFrom+ " 00:00:00"), convertStringDate(dayTo+ " 23:59:59"));
 		setListAdapter(new ReportListAdapter(activity, transactions));
 	}
-	
-	/*private void queryDateInterval(long start, long end){	
-		DatabaseHandler db = new DatabaseHandler(activity);
-		transactions = db.getTransactionsDateInterval(start, end);
-		db.close();
-	}*/
 	
 	private void queryDateIntervalPlace(long start, long end){	
 		DatabaseHandler db = new DatabaseHandler(activity);
@@ -426,71 +209,24 @@ public class ReportListAll extends SherlockListFragment {
 	}
 	
 	private void doSummary(){
-		
-		String message = "";
-		String card = "";
-		
-		Map<String, Double> sum = new HashMap<String, Double>();
-		Set<String> currs = new HashSet<String>();
-		
-		Map<String, Double> sumIncom = new HashMap<String, Double>();
-		Set<String> currsIncom = new HashSet<String>();
-		
-		for (TransactionEntry t : transactions) {
-			if(!currs.contains(t.getAmountCurr()) &&
-					(t.getType() == StaticValues.TRANSACTION_TYPE_EXPENSE)){
-				currs.add(t.getAmountCurr());
-				sum.put(t.getAmountCurr(), 0.0);
-			}
-			if(!currsIncom.contains(t.getAmountCurr()) && 
-					(t.getType() == StaticValues.TRANSACTION_TYPE_INCOME)){
-				currsIncom.add(t.getAmountCurr());
-				sumIncom.put(t.getAmountCurr(), 0.0);
-			}
-			
-			if(t.getType() == StaticValues.TRANSACTION_TYPE_EXPENSE){
-				sum.put(t.getAmountCurr(), sum.get(t.getAmountCurr())+t.getAmount());
-				card = t.getCard();
-			}else if(t.getType() == StaticValues.TRANSACTION_TYPE_INCOME){
-				sumIncom.put(t.getAmountCurr(), sumIncom.get(t.getAmountCurr())+t.getAmount());
-				card = t.getCard();
-			}
+    	if(dayFrom == null || dayTo == null){
+    		//TODO: toast something
+    	}
+    	if(transactions.isEmpty()){
+    		//TODO: toast something
+    	}
+    	DatabaseHandler db = new DatabaseHandler(activity);
+		List<TransactionEntry> trs = db.getTransactionsForGraph(convertStringDate(dayFrom+ " 00:00:00"), 
+				convertStringDate(dayTo+ " 23:59:59"), StaticValues.CURR_RUB);
+		db.close();
+		if(trs.size()<1){
+			  Toast.makeText(activity, getResources().getString(R.string.toast_no_values) + " " + StaticValues.CURR_RUB, 
+					  Toast.LENGTH_LONG).show();
 		}
-		
-		message += getResources().getString(R.string.str_period) + ": " + dayFrom + "~" + dayTo + "\r\n" + 
-				getResources().getString(R.string.str_card) + ": " + card + "\r\n" + 
-				getResources().getString(R.string.str_spent) + ": ";
-		//round sum values
-		for(String s: currs){
-			double rndSum = sum.get(s) * 100;
-			rndSum = Math.round(rndSum);
-			rndSum /=100;
-			message += ""+rndSum+ " " + s + ", "; 
-		}
-		message = message.substring(0, message.length()-2); // remove last comma[space] ", "
-		
-		if(currsIncom.size()>0){
-			message += "\r\n" + getResources().getString(R.string.str_earned) + ": ";
-			for(String s: currsIncom){
-				double rndSum = sumIncom.get(s) * 100;
-				rndSum = Math.round(rndSum);
-				rndSum /=100;
-				message += ""+rndSum+ " " + s + ", "; 
-			}
-			message = message.substring(0, message.length()-2); // remove last comma[space] ", "
-		}
-		
-		message += "\r\n"+ getResources().getString(R.string.str_tr_number) + ": " + transactions.size();
-		
-		new AlertDialog.Builder(activity)
-	    .setMessage(message)
-	    .setPositiveButton(R.string.dialog_ok, new android.content.DialogInterface.OnClickListener() {                
-	        @Override
-	        public void onClick(DialogInterface dialog, int which) {
-	                       
-	        }
-	    })
-	    .show(); 
+    	Intent myIntent = new Intent(activity, ReportSummary.class);
+    	myIntent.putExtra("day_from", dayFrom);
+    	myIntent.putExtra("day_to", dayTo);
+    	activity.startActivity(myIntent);
 	}
 	
 	  private void doSort(){
