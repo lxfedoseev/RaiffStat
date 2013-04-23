db78c1568dcd350e1cfaea672fba6c5f0a5dbe87
Alex Fedoseev <lxfedoseev@gmail.com>
2013-04-23 11:55:46 +0400

    release version

--- src/ru/almaunion/raiffstat/AppProperties.java
+++ src/ru/almaunion/raiffstat/AppProperties.java
@@ -6,7 +6,8 @@ public class AppProperties {
      * In a real app, either hide this on your server
      * or compose it of multiple concat Strings so it's not so easy to find
      */
-    public static final String BASE_64_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArFKsWOCguKlHkKngnwp3JFU/VF+ChVUNSgNlWdOXPKoW+ZEHZsWRD9CReNhiJoLvnkhlGioWyl729J9uNZF0j3qOAzTIBJpkDfYcGz8nYmUJrWcPxzGuQ7OMcIArJYJvpnd3bxafomhdqc1OzEhK/fEX26lbzh4HHvOpcFmrhfUjgkLvwzO/+mlvHAv9wUWMNplnEMrAOJkMyewz3qZuzAHY04m6OQYu/Xqp6cGtBXfntzx3UtaSY9bpjE0c3dGZQsf+4J9JtLqsGEfMf7JvAiqpBLeYwfNhYMrL7eeBlqTEN/pTkqJRsRdP24jxePWgA0VMK3R8ueh55/Tl0cvN3QIDAQAB";
+    //public static final String BASE_64_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArFKsWOCguKlHkKngnwp3JFU/VF+ChVUNSgNlWdOXPKoW+ZEHZsWRD9CReNhiJoLvnkhlGioWyl729J9uNZF0j3qOAzTIBJpkDfYcGz8nYmUJrWcPxzGuQ7OMcIArJYJvpnd3bxafomhdqc1OzEhK/fEX26lbzh4HHvOpcFmrhfUjgkLvwzO/+mlvHAv9wUWMNplnEMrAOJkMyewz3qZuzAHY04m6OQYu/Xqp6cGtBXfntzx3UtaSY9bpjE0c3dGZQsf+4J9JtLqsGEfMf7JvAiqpBLeYwfNhYMrL7eeBlqTEN/pTkqJRsRdP24jxePWgA0VMK3R8ueh55/Tl0cvN3QIDAQAB";
+	public static final String BASE_64_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1WS6TacZT+DMFe94dVE4uRntsBNhm6u9TKkXrMM7+/sWItwQDo7obrg9zf8xVxIr1UrDzuqfJK+rwc9WkbwgvhC3njPHFgTQj2w+7GqKS30InJW6WiHhcjyJVNIx5Lmv8CGGNwJA/GSo49SrNZJdZdn5tqOQ7Dq7u3BFXNrToB+9Mb/FmIPcJWDAwcIQw53rpVQcguHPS7HVDIZT++YOano9UIvU0WQB3x7UrbQnP157KND74WfhL/7B+dfdLsMLVpEcE+WEkLhOPz3+y8ZVOmNHO0pN8ZHx6+IdBYMr//SYq+yOS5ft910C84PHtdzpuPBN3gqJgGL8GSccY6cBqQIDAQAB";
     
     public static final int DONATION_AMOUNT_01 = 1; // $1
     public static final int DONATION_AMOUNT_02 = 2; // $3
--- src/ru/almaunion/raiffstat/donationui/domain/items/Donation.java
+++ src/ru/almaunion/raiffstat/donationui/domain/items/Donation.java
@@ -4,4 +4,8 @@ public class Donation {
 	
 	public static final String SKU = "android.test.purchased"; // Replace this with your item ID;
 	
+	public static final String SKU_DONATION_01 = "raiffstat.donation.01"; // $1
+	public static final String SKU_DONATION_02 = "raiffstat.donation.02"; // $3
+	public static final String SKU_DONATION_03 = "raiffstat.donation.03"; // $5
+	
 }
